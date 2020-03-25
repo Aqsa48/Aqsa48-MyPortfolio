3fa8b53c6aaad39dd62dc6ef8d652a191eb779b0
Aqsa48 <aksaumar35@gmail.com>
2020-03-25 20:01:00 +0500

    first commit

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 const express = require('express');
 const app = express();
-const path=require('path');
+const path = require('path');
 //Serves resources from public folder
-app.use(express.static(path.join(__dirname,'public'))); 
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 const fs = require('fs');
-app.get('/',(req,res)=>{
-//     let rawdata = fs.readFileSync('./Data/member.json');
-// let contributors = JSON.parse(rawdata);
-// console.log(contributors);
+app.get('/', (req, res) => {
 
     res.render('index')
 });
-app.get('/download',(req,res)=>{
+app.get('/download', (req, res) => {
     console.log(__dirname);
     var file = __dirname + '/cv_file/MyUpdatedCv.pdf';
     res.download(file);
 })
 
 //connection to server
-app.listen(3000||process.env.PORT, error => {
+app.listen(3000 || process.env.PORT, error => {
     if (error) console.log("error is " + error);
     else console.log("listening to port " + 3000);
 });
